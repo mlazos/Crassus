5773d8949a3d580094c8b027b96aa1d1d2a8eff8
Matthew B. Smith <matthew_b_smith@brown.edu>
2013-04-09 19:57:53 -0400

    changes to GUI interfaces

--- src/edu/brown/cs32/atian/crassus/gui/EventWindow.java
+++ src/edu/brown/cs32/atian/crassus/gui/EventWindow.java
@@ -2,10 +2,6 @@ package edu.brown.cs32.atian.crassus.gui;
 
 public interface EventWindow {
 
-	
-	
-	
-	
-	
+	public void setWindowCloseListener(WindowCloseListener listener);
 	
 }
---
+++ src/edu/brown/cs32/atian/crassus/gui/WindowCloseListener.java
@@ -0,0 +1,9 @@
+package edu.brown.cs32.atian.crassus.gui;
+
+public interface WindowCloseListener {
+
+	public void windowClosedWithEvent(Event e);
+	
+	public void windowClosedWithCancel();
+	
+}
