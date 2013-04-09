a5b1325773d279134ac029d66e7647c919cfa90a
Matthew B. Smith <matthew_b_smith@brown.edu>
2013-04-09 17:06:53 -0400

    some changes to StockPlot

--- src/edu/brown/cs32/atian/crassus/gui/StockPlot.java
+++ src/edu/brown/cs32/atian/crassus/gui/StockPlot.java
@@ -1,7 +1,10 @@
-/**
+/*
  * 
  */
 package edu.brown.cs32.atian.crassus.gui;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 import org.jfree.chart.JFreeChart;
 
@@ -15,18 +18,20 @@ import org.jfree.chart.JFreeChart;
  */
 public interface StockPlot {
 
-	/**
+	/*
 	 * This function allows retrieval of the primary chart, so that it can be changed or drawn to the screen
 	 * @return the primary chart, which contains the prices of the stock over time
 	 */
-	public JFreeChart getPrimaryChart();
+	//public JFreeChart getPrimaryChart();
 	
-	/**
+	/*
 	 * This function allows retrieval of the relative strength chart, so that it can be changed or drawn to the screen.
 	 * IMPORTANT, in many cases, 
 	 * @return the relative strength chart, which contains the relative strength of the stock over time
 	 */
-	public JFreeChart getRelativeStrengthChart();
+	//public JFreeChart getRelativeStrengthChart();
+	
+	public void addDatasetToPrimaryChart(Color color );
 	
 	/**
 	 * @param state indicates whether the relative strength data should be drawn on the same chart as the stock price data
@@ -36,6 +41,12 @@ public interface StockPlot {
 	/**
 	 * turns the Relative Strength chart on
 	 */
-	public void turnRsOn();
+	public void turnRsOn() throws CantTurnRsOnAfterChartsRetreivedException;
+	
+	public boolean isRsOn();
+	
+	public BufferedImage getPrimaryBufferedImage(int width, int height);
+	
+	public BufferedImage getRsBufferedImage(int width, int height);
 	
 }
