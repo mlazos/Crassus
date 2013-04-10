7bcf7d3c32cb8d45ac9eff95cbff4d69136605fb
Michael Lazos <mlazos@cs.brown.edu>
2013-04-10 15:29:07 -0400

    updated plotwrapper class

--- src/edu/brown/cs32/atian/crassus/gui/PlotWrapper.java
+++ src/edu/brown/cs32/atian/crassus/gui/PlotWrapper.java
@@ -4,6 +4,7 @@ package edu.brown.cs32.atian.crassus.gui;
 import java.awt.Paint;
 import java.awt.Color;
 import java.awt.BasicStroke;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import org.jfree.chart.JFreeChart;
 
@@ -18,40 +19,36 @@ public class PlotWrapper implements StockPlot
 	private JFreeChart primaryChart;
 	private JFreeChart rsChart;
 	
-	
+	//add a series to the list of series
+	public void addSeries(SeriesWrapper series)
+	{
+		this.series.add(series);
+	}
+
+	//set whether the relative strength chart should be separate from the primary chart
 	@Override
-	public JFreeChart getPrimaryChart() 
+	public void setRsOnSameChart(boolean isRsOnSameChart) 
+	{
+		this.isRsOnSameChart = isRsOnSameChart;
+	}
+
+	@Override
+	public boolean isRsOn() 
+	{
+		return isRsOnSameChart;
+	}
+
+	@Override
+	public BufferedImage getPrimaryBufferedImage(int width, int height) 
 	{
 		return null;
 	}
 
 	@Override
-	public JFreeChart getRelativeStrengthChart() 
+	public BufferedImage getRsBufferedImage(int width, int height) 
 	{
-		if(isRsOnSameChart)
-		{
-			throw new Error("RS chart can't be retrieved, it is set to be on the primary chart.");
-		}
-		
-		
-		return rsChart;
-	}
 
-	@Override
-	public void setRsOnSameChart(boolean state) 
-	{
-		isRsOnSameChart = true;
-	}
-
-	@Override
-	public void turnRsOn() 
-	{
-		isRsOn = true;
-	}
-	
-	public void addSeries(SeriesWrapper series)
-	{
-		this.series.add(series);
+		return null;
 	}
 	
 	
