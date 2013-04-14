355f6fba80927d070120adb39d7f80124233330a
Andrew Tian <andrew_tian@brown.edu>
2013-04-14 14:24:37 -0400

    added demark and fibonacci pivot options and added MACD and StochasticOscillator wireframes

--- src/edu/brown/cs32/atian/crassus/backend/PivotPoints.java
+++ src/edu/brown/cs32/atian/crassus/backend/PivotPoints.java
@@ -30,9 +30,9 @@ public class PivotPoints implements Indicator {
 	private List<IndicatorDatum> resistance2;
 	private List<IndicatorDatum> resistance3;
 	private List<StockTimeFrameData> data;
+	private int pivotType;
 
-	
-	public PivotPoints(List<StockTimeFrameData> data) {
+	public PivotPoints(List<StockTimeFrameData> data, String pivotOption) {
 		this.data = data;
 		pivotPoints = new ArrayList<IndicatorDatum>();
 		support1 = new ArrayList<IndicatorDatum>();
@@ -41,6 +41,22 @@ public class PivotPoints implements Indicator {
 		resistance1 = new ArrayList<IndicatorDatum>();
 		resistance2 = new ArrayList<IndicatorDatum>();
 		resistance3 = new ArrayList<IndicatorDatum>();
+		
+		switch (pivotOption) {
+		case "standard": 
+			pivotType = 0;
+			break;
+		case "fibonacci":
+			pivotType = 1;
+			break;
+		case "demark":
+			pivotType = 2;
+			break;
+		default:
+			pivotType = 0;
+			break;
+		}
+		
 		refresh(data);
 	}
 
@@ -50,9 +66,9 @@ public class PivotPoints implements Indicator {
 	}
 	
 	/**
-	 * Updates the pivot points and resistance and support lines. 
+	 * Updates the standard pivot points and resistance and support lines. 
 	 */
-	private void updateStdPivot() {
+	private void updatePivot() {
 		
 		// first value has no previous
 		for (int i = 1; i < data.size(); i++) {
@@ -60,12 +76,30 @@ public class PivotPoints implements Indicator {
 			String currTimeLabel = data.get(i).getTime();
 			
 			double pivot = (previous.getHigh() + previous.getClose() + previous.getLow()) / 3;
-			double r1 = 2 * pivot - previous.getLow();
-			double s1 = 2 * pivot - previous.getHigh();
-			double r2 = pivot + r1 - s1;
-			double s2 = pivot - (r1 - s1);
-			double s3 = previous.getLow() - 2 * (previous.getHigh() - pivot);
-			double r3 = previous.getHigh() + 2 * (pivot - previous.getLow());
+			double high = previous.getHigh();
+			double low = previous.getLow();
+			
+			
+			double s1, s2, s3, r1, r2, r3;
+			if (pivotType == 1) {								// fibonacci
+				
+				s1 = pivot - (0.832 * (high - low));
+				s2 = pivot - (0.618 * (high - low));
+				s3 = pivot - (high - low);
+				r1 = pivot - (0.382 * (high - low));
+				r2 = pivot + (0.618 * (high - low));
+				r3 = pivot + (high - low);
+			
+			} else {											// standard pivot
+			
+				r1 = 2 * pivot - low;
+				s1 = 2 * pivot - high;
+				r2 = pivot + r1 - s1;
+				s2 = pivot - (r1 - s1);
+				s3 = low - 2 * (high - pivot);
+				r3 = high + 2 * (pivot - low);
+			
+			}
 			
 			pivotPoints.add(new IndicatorDatum(currTimeLabel, pivot));
 			support1.add(new IndicatorDatum(currTimeLabel, s1));
@@ -78,10 +112,48 @@ public class PivotPoints implements Indicator {
 		}
 	}
 	
+	/**
+	 * Updates demark pivot points; differ from above in that demark only
+	 * has one resistance and one support line from the pivot.
+	 */
+	private void updateDemark() {
+		
+		// first value has no previous
+		for (int i = 1; i < data.size(); i++) {
+			StockTimeFrameData previous = data.get(i - 1);
+			String currTimeLabel = data.get(i).getTime();
+			
+			double pivot = (previous.getHigh() + previous.getClose() + previous.getLow()) / 3;
+			double high = previous.getHigh();
+			double low = previous.getLow();
+			
+			double close = previous.getClose();
+			double open = previous.getOpen();
+			double x;
+			if (close < open) {
+				x = high + (2 * low) + close;
+			} else if (close > open) {
+				x = (2 * high) + low + close;
+			} else {
+				x = high + low + (2 * close);
+			}
+			
+			pivot = x / 4;
+			double s1 = x/2 - high;
+			double r1 = x/2 - low;
+			
+			pivotPoints.add(new IndicatorDatum(currTimeLabel, pivot));
+			support1.add(new IndicatorDatum(currTimeLabel, s1));
+			resistance1.add(new IndicatorDatum(currTimeLabel, r1));
+		}
+	}
+	
 
 	@Override
 	public void refresh(List<StockTimeFrameData> data) {
 		this.data = data;
-		updateStdPivot();
+		
+		if (pivotType == 2) updateDemark();
+		else updatePivot();
 	}
 }
