cc18c716a1483f795e31b688771297df6aab0cb1
Michael Lazos <mlazos@cs.brown.edu>
2013-04-28 18:36:55 -0400

    added main event frame functionality with dynamic resizing

--- src/edu/brown/cs32/atian/crassus/gui/EventWindowFrame.java
+++ src/edu/brown/cs32/atian/crassus/gui/EventWindowFrame.java
@@ -3,6 +3,8 @@ package edu.brown.cs32.atian.crassus.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
@@ -12,6 +14,8 @@ import javax.swing.border.EmptyBorder;
 
 public class EventWindowFrame implements EventWindow {
 
+	private JPanel currentPanel;
+	
 	private JFrame frame;
 	/**
 	 * @param args
@@ -30,14 +34,15 @@ public class EventWindowFrame implements EventWindow {
 
 	public EventWindowFrame()
 	{
+		currentPanel = new BolingerBandPanel();
+		
 		//add dropdown to main Frame
 		JPanel[] eventList = {new BolingerBandPanel(), new MACDPanel(), new PivotPanel(), new PriceChannelPanel(), new RSIPanel(), new StochOscillPanel()};
 		JComboBox<JPanel> selectEvent = new JComboBox<JPanel>(eventList);
-		selectEvent.setSelectedIndex(0); 
-		//selectEvent.setPreferredSize(new Dimension(200,20));
+		selectEvent.addActionListener(new WindowChanger());
 		JPanel dropDownPanel = new JPanel();
-		//dropDownPanel.setMinimumSize(new Dimension(200,20));
-		//dropDownPanel.setMaximumSize(new Dimension(200, 20));
+		dropDownPanel.setMinimumSize(new Dimension(250,20));
+		dropDownPanel.setMaximumSize(new Dimension(250,20));
 		dropDownPanel.add(selectEvent);
 		dropDownPanel.setBorder(new EmptyBorder(20,20,20,20));
 		
@@ -48,8 +53,34 @@ public class EventWindowFrame implements EventWindow {
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setLayout(new BorderLayout());
 		frame.add(dropDownPanel, BorderLayout.NORTH);
-		frame.add(new BolingerBandPanel(), BorderLayout.CENTER);
+		frame.add(currentPanel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		selectEvent.setSelectedIndex(0); 
+	}
+	
+	class WindowChanger implements ActionListener
+	{
+		public WindowChanger(){}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+			@SuppressWarnings("unchecked")
+			JComboBox<JPanel> selectEvent = (JComboBox<JPanel>)e.getSource();
+			JPanel newPanel = (JPanel)selectEvent.getSelectedItem();
+			Dimension panelDim = newPanel.getSize();
+			frame.remove(currentPanel);
+			currentPanel = newPanel;
+			frame.add(currentPanel, BorderLayout.CENTER);
+			frame.setSize(500, 60 + panelDim.height);
+			frame.setMinimumSize(new Dimension(500, 60 + panelDim.height));
+			frame.pack();
+			frame.setVisible(true);
+			
+		}
+		
 	}
 	
 	@Override
